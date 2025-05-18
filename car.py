970a753a0076a4f16203e830e34e057793d4d5f2
collins muriuki <muriukicollins48@gmail.com>
2025-05-18 20:32:12 +0300

    final comit

---
+++ car.py
@@ -0,0 +1,8 @@
+from vehicle import Vehicle
+
+class Car(Vehicle):
+    def __init__(self, wheel_size, wheel_number):
+        super().__init__(wheel_size, wheel_number)
+
+    def go(self):
+        return "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
